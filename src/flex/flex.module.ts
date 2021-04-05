78d8e4e4ef9c3316d208dd132e29cc96a107919f
elyassgh <elghazi.elyass@gmail.com>
2021-04-05 19:33:01 +0100

    local code

--- src/flex/flex.module.ts
+++ src/flex/flex.module.ts
@@ -1,32 +1,39 @@
-import { EventController } from './controllers/event.controller';
 import { JourDao } from './dao/jourdao';
 import { JourService } from './services/jour.service';
-import { ProfilController } from './controllers/profil.controller';
-import { ProfesseurDao } from './dao/professeur.dao';
+import { EventController } from './controllers/event.controller';
+
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProfesseurController } from './controllers/professeur.controller';
-import { ProfesseurService } from './services/professeur.service';
-import { EtudiantDao } from './dao/etudiantdao';
-import { EtudaintController } from './controllers/etudiant.controller';
-import { EtudaintService } from './services/etudiant.service';
+
+
 import { ProfilDao } from './dao/profil.dao';
 import { ProfilService } from './services/profil.service';
+import { ProfilController } from './controllers/profil.controller';
+
+import { ProfesseurDao } from './dao/professeur.dao';
+import { ProfesseurService } from './services/professeur.service';
+import { ProfesseurController } from './controllers/professeur.controller';
+
+import { EtudiantDao } from './dao/etudiantdao';
+import { EtudaintService } from './services/etudiant.service';
+import { EtudaintController } from './controllers/etudiant.controller';
+
 import { FiliereDao } from './dao/filiere.dao';
 import { FiliereService } from './services/filiere.service';
 import { FiliereControlller } from './controllers/filiere.controller';
 
 import { PorteDao } from './dao/porte.dao';
-import { SalleDao } from './dao/salle.dao';
 import { PorteService } from './services/porte.service'
-import { SalleService } from './services/salle.service'
 import { PorteController } from './controllers/porte.controller';
+
+import { SalleDao } from './dao/salle.dao';
+import { SalleService } from './services/salle.service'
 import { SalleController } from './controllers/salle.controller';
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([ProfesseurDao, EtudiantDao, ProfilDao, FiliereDao, PorteDao, SalleDao])],
-    controllers: [ProfesseurController, EtudaintController, ProfilController, FiliereControlller, PorteController, SalleController],
+    imports: [TypeOrmModule.forFeature([ProfesseurDao, EtudiantDao, ProfilDao, FiliereDao, PorteDao, SalleDao, JourDao])],
+    controllers: [ProfesseurController, EtudaintController, ProfilController, FiliereControlller, PorteController, SalleController, EventController],
     providers: [ProfesseurService, EtudaintService, ProfilService, FiliereService, PorteService, SalleService]
 })
 export class FlexModule { }
