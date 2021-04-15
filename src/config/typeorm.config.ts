236aa3e04740514151d6dbb27cb3beebf4042b18
AbidarYassine <71972349+AbidarYassine@users.noreply.github.com>
2021-04-15 00:25:32 +0000

    add deploy by @AbidarYassine

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,6 +1,8 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmOption: TypeOrmModuleOptions = {
+
+    // local db
     // type: 'mysql',
     // host: 'localhost',
     // port: 3306,
@@ -10,6 +12,9 @@ export const typeOrmOption: TypeOrmModuleOptions = {
     // entities: [__dirname + "/../**/*.entity{.ts,.js}"],
     // synchronize: false,
 
+
+
+    // prod db
     type: 'mysql',
     host: 'us-cdbr-east-03.cleardb.com',
     port: 3306,
@@ -17,7 +22,7 @@ export const typeOrmOption: TypeOrmModuleOptions = {
     password: '4af49fcc',
     database: 'heroku_3d19fa44bbd5108',
     entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-    synchronize: true,
+    synchronize: false,
 
 
 
